8f565e5ebc01fed62d1985e754abb8d3bf3944e8
Todd Riemenschneider <twr14152@gmail.com>
2023-02-12 11:17:12 -0500

    Create Hello.java

---
+++ ch1/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		System.out.printf("%s\n", "This is a test.");
+		System.out.println(System.out);
+	}
+}
